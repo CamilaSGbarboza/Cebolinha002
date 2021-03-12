46a3de4e05960efb29837fbff6774483f3004971
camila <camilasangobar@gmail.com>
2021-03-12 16:12:11 -0300

    cebolinha

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cebolinha002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("digite:");
+            string cebolinha = Console.ReadLine();
+            Console.WriteLine($"{cebolinha.Replace("r", "l")}");
+        }
+    }
+}
